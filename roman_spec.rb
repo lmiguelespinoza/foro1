fb4e575c2d94a618cb2938f4c1f5157ebd45720c
lmiguelespinoza <lmiguelespinoza@gmail.com>
2012-09-09 17:18:32 -0500

    Prueba 1..19

--- roman_spec.rb
+++ roman_spec.rb
@@ -5,6 +5,9 @@ describe Roman do
   before do
     @roman = Roman.new
   end
+  it 'should convert 19 to XIX' do    
+    @roman.convert(19).must_equal "XIX" #assert
+  end
   it 'should convert 18 to XVIII' do    
     @roman.convert(18).must_equal "XVIII" #assert
   end
