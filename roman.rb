07298817ed221008a00f2fcb89fde17ed23fdc04
lmiguelespinoza <lmiguelespinoza@gmail.com>
2012-09-09 16:10:08 -0500

    Prueba 1..3

--- roman.rb
+++ roman.rb
@@ -1,8 +1,7 @@
 class Roman
 
   def convert(number)  	
-    return "II" if number==2
-    return "I" if number==1
+    return "I"*number
   end
 
  end
--- roman_spec.rb
+++ roman_spec.rb
@@ -5,10 +5,13 @@ describe Roman do
   before do
     @roman = Roman.new
   end
-  it 'should convert 1 to I' do    
-    @roman.convert(1).must_equal "I" #assert
+  it 'should convert 3 to III' do    
+    @roman.convert(3).must_equal "III" #assert
   end
   it 'should convert 2 to II' do    
     @roman.convert(2).must_equal "II" #assert
   end
+  it 'should convert 1 to I' do    
+    @roman.convert(1).must_equal "I" #assert
+  end  
 end
