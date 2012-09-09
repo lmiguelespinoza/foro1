1056e8fce69c3558706d8f93139fd51a4c9c9a83
lmiguelespinoza <lmiguelespinoza@gmail.com>
2012-09-09 16:07:09 -0500

    Prueba 1-I,2-II

--- roman.rb
+++ roman.rb
@@ -1,7 +1,8 @@
 class Roman
 
-  def convert(number)
-    return "I"
+  def convert(number)  	
+    return "II" if number==2
+    return "I" if number==1
   end
 
  end
--- roman_spec.rb
+++ roman_spec.rb
@@ -5,9 +5,10 @@ describe Roman do
   before do
     @roman = Roman.new
   end
-
-	it 'should convert 1 to I' do    
+  it 'should convert 1 to I' do    
     @roman.convert(1).must_equal "I" #assert
   end
-
+  it 'should convert 2 to II' do    
+    @roman.convert(2).must_equal "II" #assert
+  end
 end
