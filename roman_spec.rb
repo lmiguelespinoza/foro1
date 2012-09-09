e91f991ff813bd3f21e2534a42dc27c72b3014af
lmiguelespinoza <lmiguelespinoza@gmail.com>
2012-09-09 17:14:35 -0500

    Prueba 1..15

--- roman_spec.rb
+++ roman_spec.rb
@@ -5,6 +5,9 @@ describe Roman do
   before do
     @roman = Roman.new
   end
+  it 'should convert 15 to XV' do    
+    @roman.convert(15).must_equal "XV" #assert
+  end
   it 'should convert 14 to XIV' do    
     @roman.convert(14).must_equal "XIV" #assert
   end
